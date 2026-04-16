6985c61faea5b4bb440560d8ff012701d2e71173
Nikhil5922 <nikhilkumar7800@gmail.com>
2026-04-16 13:11:24 +0530

    add documentation for useGenericViewProperty hook to clarify usage and return value

--- frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/pt/useGenericViewProperty.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/pt/useGenericViewProperty.js
@@ -1,6 +1,25 @@
 import { queryTemplate } from "../../common/queryTemplate";
 import { PTSearch } from "../../services/molecules/PT/Search";
 
+/**
+ * Custom hook which can be used to
+ * get the property search using property id and tenant id 
+ * and return the property generic template to show employee and citizen view
+ * 
+ * 
+ * @example
+ *  Digit.Hooks.pt.useGenericViewProperty(t,
+ *                                        tenantId, 
+ *                                        propertyId,
+ *                                        { // all configs supported by the usequery 
+ *                                                  default:(data)=>{
+ *                                                                   format
+ *                                                                   return formattedData;
+ *                                                                   }
+ *                                        });
+ * 
+ * @returns {Object} Returns the object of the useQuery from react-query.
+ */
 const useGenericViewProperty = (t, tenantId, propertyIds, config = {}, userType) => {
   const defaultSelect = (data) => {
     return { ...data };
